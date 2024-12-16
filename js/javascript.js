67813b9e3d49016b15deb1c98b48b15e08307266
RebecaMonagas <rebeca30127@gmail.com>
2024-12-16 15:13:10 -0300

    creacion de boton agregar al carrito

--- js/javascript.js
+++ js/javascript.js
@@ -1,4 +1,69 @@
-console.log("Hola Mundo!");
+// var usuario = prompt("ingrese su nombre:", "");
+// document.write("Hola " + usuario);
+
+// let numero1 = prompt("Ingrese un numero: ")
+// let numero2 = prompt("Ingrese un numero: ")
+// let suma = Number(numero1) + Number(numero2)
+// document.write("La suma es: " + suma)
+// console.log(typeof suma)
+
+// let isLogged = false
+
+// console.error("Esto es un error")
+// console.warn("Esto es una advertencia")
+
+// const nombre = "Humberto"
+// console.log(nombre)
+
+// for (let index = 0; index < 1; index++) {
+//   console.log(saludo)
+//   var numero = 1
+// }
+
+// console.log(numero)
 
 
-var usuario = prompt("ingrese su nombre: ");
+// console.log("Hola Mundo!");
+// const button = document.querySelector('#button')
+// const container = document.querySelector('.container')
+
+// button.addEventListener('click', () => {
+//   container.innerHTML = `<p class="hola">Hola mundo</p>`
+// })
+
+
+// //0 1  2
+// const array = ["manzana", "pera", "banana", "rebeca"]; // 3 elementos
+// const array2 = ["dove", "higienol", "garnier", "loreal"]
+// console.log("longitud del array2", array2.length)
+// console.log(array2[2])
+
+// for (let index = 0; index < array2.length; index++){
+//   console.log(array2[index])
+// }
+
+
+// ///
+// const array3 = ["Blusa", "Vestido", "Pantalón", "Falda", "Chaqueta", "Jeans", "Blazer", "Mochila", "Sombrero", "Zapatillas", "Sandalias", "Bolso", "bufanda", "Reloj", "Sudadera", "Camisa", "Camiseta", "Musculosa", "Short", "Pulsera"]
+// console.log("longitud del array3", array3.length)
+// console.log(array3[19])
+
+// for (let index = 0; index < array3.length; index++){
+//   console.log(array3[index])
+// }
+
+// const saludo = "😃";
+// console.log("longitud de un string o text", saludo.length);
+// console.log("longitud del array", array.length);
+
+// for (let index = 0; index < 3; index++) {
+// 	console.log(`${array[index]} estamos en la posicion ${index}`)
+// }
+
+// primera vuelta 0, se imprime el que esta en la posicion 0 que es el 1
+// segunda vuelta 1, se imprime el que esta en la posicion 2
+
+
+
+
+
